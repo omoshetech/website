61aceb9a59c1b5e21ea9fe26e037be847bc5a7f7
Takuya Tsuchida <omoshetech.t+git@gmail.com>
2015-01-13 01:32:16 +0900

    Add a "Read More" link to a summary

--- web/app/themes/omoshetech/lib/extras.php
+++ web/app/themes/omoshetech/lib/extras.php
@@ -6,3 +6,11 @@ function roots_excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'roots') . '</a>';
 }
 add_filter('excerpt_more', 'roots_excerpt_more');
+
+/**
+ * Prevent page scroll when clicking the more link
+ */
+function remove_more_link_scroll($link) {
+	return preg_replace('|#more-[0-9]+|', '', $link);
+}
+add_filter('the_content_more_link', 'remove_more_link_scroll');
