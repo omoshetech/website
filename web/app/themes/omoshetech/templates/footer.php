dcb443457e2779ca21f269a19686a439242c8458
Takuya Tsuchida <omoshetech.t+git@gmail.com>
2015-01-18 08:14:18 +0900

    Add a copyright

--- web/app/themes/omoshetech/templates/footer.php
+++ web/app/themes/omoshetech/templates/footer.php
@@ -1,6 +1,6 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
-    <p>&copy; 2015 OmosheTech</p>
+    <p id="copyright">&copy; 2015 OmosheTech</p>
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
