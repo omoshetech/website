b26af1fdf7a86f8c6b54561fbf74ffdce0d9eab8
Takuya Tsuchida <omoshetech.t+git@gmail.com>
2015-01-18 08:04:08 +0900

    Remove the relative url feature of Soil

--- web/app/themes/omoshetech/lib/config.php
+++ web/app/themes/omoshetech/lib/config.php
@@ -3,7 +3,6 @@
  * Enable theme features
  */
 add_theme_support('soil-clean-up');         // Enable clean up from Soil
-add_theme_support('soil-relative-urls');    // Enable relative URLs from Soil
 add_theme_support('soil-nice-search');      // Enable /?s= to /search/ redirect from Soil
 add_theme_support('soil-disable-trackbacks');
 add_theme_support('soil-disable-asset-versioning');
