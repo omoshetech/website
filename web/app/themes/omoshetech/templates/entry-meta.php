4277fc727d21e79bab445d8e04c2438d94ff2e6e
Takuya Tsuchida <omoshetech.t+git@gmail.com>
2015-01-21 06:15:51 +0900

    Fix a featured image size

--- web/app/themes/omoshetech/templates/entry-meta.php
+++ web/app/themes/omoshetech/templates/entry-meta.php
@@ -1,5 +1,5 @@
 <?php
-  the_post_thumbnail('large', array('class' => 'img-responsive'));
+  the_post_thumbnail('full', array('class' => 'img-responsive'));
 ?>
 <time class="updated" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 <div class="addthis_sharing_toolbox pull-right" data-url="<?php echo get_permalink(); ?>" data-title="<?php the_title_attribute(); ?>"></div>
