a90304d22fcbbf616c6e96847b3c1b3093a2ecec
Takuya Tsuchida <omoshetech.t+git@gmail.com>
2015-01-21 03:14:28 +0900

    Design the main area

--- web/app/themes/omoshetech/templates/content.php
+++ web/app/themes/omoshetech/templates/content.php
@@ -2,10 +2,16 @@
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   </header>
-  <div class="entry-meta">
-    <?php get_template_part('templates/entry-meta'); ?>
-  </div>
-  <div class="entry-summary">
-    <?php the_content('続きを読む'); ?>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="entry-meta">
+        <?php get_template_part('templates/entry-meta'); ?>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="entry-summary">
+        <?php the_content('続きを読む'); ?>
+      </div>
+    </div>
   </div>
 </article>
