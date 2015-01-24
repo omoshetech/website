67036eae05fc8f9f3e325f10fede39afb9334987
Takuya Tsuchida <omoshetech.t+git@gmail.com>
2015-01-24 22:04:35 +0900

    Add a filter to disable moving checked category items

--- web/app/themes/omoshetech/lib/extras.php
+++ web/app/themes/omoshetech/lib/extras.php
@@ -8,9 +8,18 @@ function roots_excerpt_more() {
 add_filter('excerpt_more', 'roots_excerpt_more');
 
 /**
- * Prevent page scroll when clicking the more link
+ * Disable page scroll when clicking the more link
  */
-function remove_more_link_scroll($link) {
+function disable_more_link_scroll($link) {
 	return preg_replace('|#more-[0-9]+|', '', $link);
 }
-add_filter('the_content_more_link', 'remove_more_link_scroll');
+add_filter('the_content_more_link', 'disable_more_link_scroll');
+
+/**
+ * Disable moving checked items out of the hierarchy and to the top of the list
+ */
+function disable_checked_ontop($args, $post_id = 0) {
+  $args['checked_ontop'] = false;
+  return $args;
+}
+add_filter('wp_terms_checklist_args', 'disable_checked_ontop');
