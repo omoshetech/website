e0ddced2bdddf2697da8b70da1ee6fc85e758909
Takuya Tsuchida <omoshetech.t+git@gmail.com>
2015-03-18 03:42:13 +0900

    Add the search template

---
+++ web/app/themes/omoshetech/search.php
@@ -0,0 +1,14 @@
+<?php get_template_part('templates/breadcrumbs'); ?>
+<?php get_template_part('templates/page', 'header'); ?>
+
+<?php if (!have_posts()) : ?>
+  <div class="alert alert-warning">
+    <?php _e('Sorry, no results were found.', 'roots'); ?>
+  </div>
+<?php endif; ?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/content', get_post_format()); ?>
+<?php endwhile; ?>
+
+<?php get_template_part('templates/pagination');
